25a68e4cc71eda1341db7224aa7a70fd5a940d6a
zylch11 <waqas.ahmed11@protonmail.com>
2022-05-06 02:48:40 +0500

    Added Sanity client

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,14 @@ import PageLayout from "Components/PageLayout";
 import CardListItem from "Components/CardListItem";
 import CardItem from "Components/CardItem";
 
-export default function Home() {
+import { getAllBlogs } from 'lib/api';
+
+export default function Home({blogs}) {
   return (
     <PageLayout>
       <AuthorIntro />
       <hr />
+      {/* {JSON.stringify(blogs)} */}
         <Row className="mb-5">
           <Col md="10">
             <CardListItem />
@@ -19,4 +22,13 @@ export default function Home() {
         </Row>
     </PageLayout>
   );
+}
+
+export async function getStaticProps() {
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs
+    }
+  }
 }
