c45f12323170bbd96499ed89841d314a927a39cb
zylch11 <waqas.ahmed11@protonmail.com>
2022-05-08 15:46:14 +0500

    Updated author Intro

--- Components/AuthorIntro.js
+++ Components/AuthorIntro.js
@@ -6,19 +6,18 @@ const AuthorIntro = () => {
         <Col md="8">
           {/* AUTHOR INTRO STARTS */}
           <Media className="mb-4 admin-intro">
-            <Image
+            {/* <Image
               roundedCircle
               width={64}
               height={64}
               className="mr-3"
               src="https://avatars1.githubusercontent.com/u/9482724?s=460&u=69a6acab13fd5547a4e316e496b573271077147f&v=4"
               alt="Generic placeholder"
-            />
+            /> */}
             <Media.Body>
-              <h5 className="font-weight-bold mb-0">Hello Friends,</h5>
+              <h5 className="font-weight-bold mb-0">Hello there,</h5>
               <p className="welcome-text">
-                My name is Filip Jerga and I am an experienced software engineer
-                and freelance developer. and this is my blog page.
+                My name is Waqas Ahmed. I am an experienced full stack web engineer and freelancer. This website is my internet home.
               </p>
             </Media.Body>
           </Media>
